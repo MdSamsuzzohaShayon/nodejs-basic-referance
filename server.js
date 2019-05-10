3de984bbeb6c4e776abee05d2600621814458b2d
Md Shayon <mdshayon0@gmail.com>
2019-05-10 10:34:06 +0600

    query string with express

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
+const express = require('express');
+let app = express();
 
-//CUSTOM MODULE
+app.set('view engine','ejs');
+app.use('/assets',express.static('assets'));
 
-const stuff = require('./stuff');
-console.log(stuff.counter(['shayon', 'khokon', 'tanvir']));
-console.log(stuff.adder(3+4));
-console.log(stuff.adder(stuff.pi, 6));
+
+
+
+
+
+app.get('/',(req, res)=>{
+    res.render('index');
+});
+app.get('/news',(req, res)=>{   
+    res.render('news',{qs: req.query});
+});
+
+
+
+
+
+
+app.get('/profile/:name',(req, res)=>{
+    let data = {
+        age: 22,
+        job:'developer',
+        hobbies: ['eating', 'fighting','Fishing']
+        };
+    //LOOKING VIEWS FOLDER THAT IS DEFAULT BEHAVIOR
+    res.render('profile.ejs',{person: req.params.name, data:data});
+    //res.render('profile.ejs');
+});
+
+
+app.listen(3000);
 
 
