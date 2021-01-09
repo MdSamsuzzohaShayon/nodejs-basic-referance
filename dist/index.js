df99f4f42c4bcfc96551fec5609c9c81eb5bf106
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2021-01-09 17:26:15 +0600

    simple functions

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 // const app: express.Application = express();
 const app = express_1.default();
+const add = (a, b) => a + b;
+// https://www.npmjs.com/package/ts-node
 app.get('/', (req, res, next) => {
+    console.log(add(5, 6));
     res.send("server is running");
 });
 app.listen(5000, () => console.log("Server is running on port 5000"));
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import express, {Application, Request, Response, NextFunction} from 'express';
 // const app: express.Application = express();
 const app: Application = express();
 
+const add = (a: number, b: number): number => a+b;
+
 
 // https://www.npmjs.com/package/ts-node
 app.get('/', (req: Request, res:Response, next:NextFunction) => {
+    console.log(add(5, 6));
     res.send("server is running");
 });
 
